b89455bcad2012d67c80f9e433887d9d0659822b
AngeloVSO <angelo_odwyer@hotmail.com>
2025-03-31 21:07:13 -0300

    Cria controller

---
+++ SimuladorEmprestimo/Controllers/SimuladorEmprestimoController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimuladorEmprestimo.Services;
+
+namespace SimuladorEmprestimo.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class SimuladorEmprestimoController(IEmprestimoService service) : ControllerBase
+{
+    private readonly IEmprestimoService _service = service;
+
+    [HttpGet("simular")]
+    public object Get([FromQuery] string nome)
+    {
+        if (string.IsNullOrWhiteSpace(nome))
+        {
+            return Results.BadRequest("O nome deve ser informado.");
+        }
+
+        var result = _service.SimularEmprestimo(nome);
+
+        return Ok(result);
+    }
+}
